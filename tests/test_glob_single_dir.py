9a86c65c427d29b75eccb0e6890e6a8129916883
Marek Kurdej <marek@quasardb.net>
2017-10-04 15:08:14 +0200

    Fix test_glob_single_dir.

--- tests/test_glob_single_dir.py
+++ tests/test_glob_single_dir.py
@@ -9,11 +9,12 @@ import run_clang_format  # pylint: disable=C0413
 
 class GlobSingleDir(unittest.TestCase):
     def test_basic(self):
-        args = run_clang_format.parse_args([sys.argv[0]])
+        args = run_clang_format.parse_args(
+            [sys.argv[0], '-d', 'glob_single_dir'])
         files = run_clang_format.glob_files(args)
         self.assertEqual(2, len(files))
-        self.assertEqual([os.path.join(os.getcwd(), 'glob_single_dir', 'a.cpp'),
-                          os.path.join(os.getcwd(), 'glob_single_dir', 'a.hpp')],
+        self.assertEqual([os.path.join('glob_single_dir', 'a.cpp'),
+                          os.path.join('glob_single_dir', 'a.hpp')],
                          files)
 
 
