f9dbbac64fea7a0e80863c10243234d4671c9d7a
Marek Kurdej <marek@quasardb.net>
2017-10-04 14:52:14 +0200

    Add glob_single_dir test.

---
+++ tests/test_glob_single_dir.py
@@ -0,0 +1,23 @@
+# pylint: disable=C0103,C0111
+
+import os
+import sys
+import unittest
+sys.path.append(os.path.abspath('..'))
+import run_clang_format  # pylint: disable=C0413
+
+
+class GlobSingleDir(unittest.TestCase):
+    def test_basic(self):
+        args = run_clang_format.parse_args(['script.py'])
+        files = run_clang_format.glob_files(args)
+        self.assertEqual(2, len(files))
+        self.assertEqual([os.path.join(os.getcwd(), 'glob_single_dir', 'a.cpp'),
+                          os.path.join(os.getcwd(), 'glob_single_dir', 'a.hpp')],
+                         files)
+
+
+if __name__ == '__main__':
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(  # pylint: disable=E1102
+        output='test-reports'))()
