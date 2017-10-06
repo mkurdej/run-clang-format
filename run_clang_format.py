aa70a8f8db69f8fcc7322e80fd45820cf54f6d5d
Marek Kurdej <marek@quasardb.net>
2017-10-06 15:59:14 +0200

    Run clang-format in multiple threads. Add -j parameter.

--- run_clang_format.py
+++ run_clang_format.py
@@ -10,8 +10,11 @@ from __future__ import print_function
 import argparse
 import fnmatch
 import os
+import multiprocessing
+import queue
 import subprocess
 import sys
+import threading
 
 
 def glob_files(args):
@@ -49,11 +52,15 @@ def parse_args(argv=None):
                         help='do not format files inplace, but write output to the console'
                         ' (useful for debugging)',
                         default=True)
+    parser.add_argument('-j', type=int, default=0,
+                        help='number of clang-format instances to be run in parallel')
 
     args = parser.parse_args(argv[1:])
 
     if not args.directories:
         args.directories = [os.getcwd()]
+
+    check_clang_format_binary(args)
 
     return args
 
@@ -68,25 +75,62 @@ def _get_format_invocation(args, filename):
     return invocation
 
 
-def format_all(args, files):
-    formatted_files = {}
-    for filename in files:
+def check_clang_format_binary(args):
+    """Checks if invoking supplied clang-format binary works."""
+    try:
+        subprocess.check_output([args.clang_format_binary, '--version'])
+    except OSError:
+        print('Unable to run clang-format. Is clang-format '
+              'binary correctly specified?', file=sys.stderr)
+        raise
+
+
+def run_format(args, task_queue, formatted_files):
+    """Takes filenames out of queue and runs clang-format on them."""
+    while True:
+        filename = task_queue.get()
         invocation = _get_format_invocation(args, filename)
-        try:
-            print('Processing {}'.format(filename), file=sys.stderr)
-            formatted = subprocess.check_output(invocation)
-            formatted_files[filename] = formatted
-        except OSError:
-            print("Cannot find clang-format at '{}'.".format(args.clang_format_binary),
-                  file=sys.stderr)
 
-            raise
-        except subprocess.CalledProcessError as ex:
-            print("Running clang-format failed with non-zero status.", file=sys.stderr)
-            print("Command    : {}".format(' '.join(ex.cmd)), file=sys.stderr)
-            print("Return code: {}".format(str(ex.returncode)), file=sys.stderr)
+        print('Processing {}'.format(filename), file=sys.stderr)
+        formatted = subprocess.check_output(invocation)
+        formatted_files[filename] = formatted
 
-            raise
+        task_queue.task_done()
+
+
+def format_all(args, files):
+    max_task = args.j
+    if max_task == 0:
+        max_task = multiprocessing.cpu_count()
+
+        formatted_files = {}
+
+    try:
+        # Spin up a bunch of format-launching threads.
+        task_queue = queue.Queue(max_task)
+        for _ in range(max_task):
+            task_thread = threading.Thread(target=run_format,
+                                           args=(args, task_queue, formatted_files))
+            task_thread.daemon = True
+            task_thread.start()
+
+        # Fill the queue with files.
+        for name in files:
+            task_queue.put(name)
+
+        # Wait for all threads to be done.
+        task_queue.join()
+
+    except OSError:
+        print("Cannot find clang-format at '{}'.".format(args.clang_format_binary),
+              file=sys.stderr)
+        raise
+
+    except subprocess.CalledProcessError as ex:
+        print("Running clang-format failed with non-zero status.", file=sys.stderr)
+        print("Command    : {}".format(' '.join(ex.cmd)), file=sys.stderr)
+        print("Return code: {}".format(str(ex.returncode)), file=sys.stderr)
+        raise
 
     return formatted_files
 
--- tests/test_parse_clang_format_binary.py
+++ tests/test_parse_clang_format_binary.py
@@ -9,15 +9,10 @@ import run_clang_format  # pylint: disable=C0413
 
 class ParseClangFormatBinary(unittest.TestCase):
     def test_basic(self):
-        args = run_clang_format.parse_args([
+        self.assertRaises(OSError, run_clang_format.parse_args, [
             sys.argv[0],
             '--clang-format-binary', 'unexisting-clang-format',
             '-d', 'parse_clang_format_binary'])
-        files = run_clang_format.glob_files(args)
-
-        self.assertEqual(1, len(files))
-
-        self.assertRaises(OSError, run_clang_format.format_all, args, files)
 
 
 if __name__ == '__main__':
